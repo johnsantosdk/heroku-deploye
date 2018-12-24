70ebc810975278aa93213fe81e112eff7ea48468
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2018-12-24 11:10:42 -0300

    test do banco de dados

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/menu', function () {
     return 'Você está no MENU';
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
