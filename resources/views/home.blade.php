b1613a2f6ff5d2baedfbe45bc48723ba2d41489c
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2019-01-22 16:45:59 -0300

    preparação para conectar ao db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Olá, Você!!!
                 </div>
             </div>
         </div>
